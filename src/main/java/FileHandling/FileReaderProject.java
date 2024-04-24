89e9e675678d752074e26d782aa5f59d75b0e227
Gaurav Kapatia <gkapatia@gmail.com>
2024-04-24 14:25:24 +0530

    whole boilerplate code

---
+++ src/main/java/FileHandling/FileReaderProject.java
@@ -0,0 +1,14 @@
+package FileHandling;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class FileReaderProject {
+    public static void main(String[] args) {
+        readFromFile("dummy.txt");
+    }
+
+    public static void readFromFile(String fileName) {
+        // you code goes here
+    }
+}
